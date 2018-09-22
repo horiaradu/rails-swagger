6d92a775102b18ffaf806121d9d4146c79881240
Horia Radu <horia.radu23@gmail.com>
2018-09-22 11:24:55 +0300

    Manually add swagger

---
+++ app/controllers/swagger_controller.rb
@@ -0,0 +1,10 @@
+class SwaggerController < ActionController::Base
+  def swagger_conf
+  end
+
+  def editor
+  end
+
+  def swagger
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :beers
+
+  get 'swagger' => 'swagger#swagger'
+  get 'swagger/config' => 'swagger#swagger_conf'
+  get 'swagger/editor' => 'swagger#editor'
 end
